9e05057cc9332bba3bd836ec81e43fa21286ea8c
Paola Vilaseca <pao_anahi_8@hotmail.com>
2022-11-13 21:41:09 -0400

    first commit

---
+++ tareaSQL.sql
@@ -0,0 +1,18 @@
+create table prueba(
+    person varchar(255),
+    supervisor varchar (255)
+);
+
+
+Insert INTO [prueba]
+(
+    [person],
+    [supervisor]
+)
+
+values(
+    ('Bob','Alice'),
+    ('Mary','Susan'),
+    ('Alice','David'),
+    ('David','Mary'),
+);
